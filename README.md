7f3340eac95a7e98ae7f75369464967c38228e93
Simon Karman <mail@simonkarman.nl>
2022-02-09 11:45:06 +0100

    Added GCP ranges

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-# AWS IP Range Count
+# Cloud Provider IP Ranges
 
-Calculates statistics about the number of ipv4 addresses and the number of cidr ranges that AWS has reserved. The data is based on the information from this page: [aws-ip-ranges](https://docs.aws.amazon.com/general/latest/gr/aws-ip-ranges.html).
+Calculates statistics about the number of ipv4 addresses and the number of cidr ranges that Cloud Providers have reserved. The data is based on the information from the following pages:
+- Amazone Web Services: [aws-ip-ranges](https://docs.aws.amazon.com/general/latest/gr/aws-ip-ranges.html)
+- Google Cloud: [find_ip_range](https://cloud.google.com/compute/docs/faq#find_ip_range)
 
 To run the analysis run the following commands from the root of this repository:
 ```bash
-curl https://ip-ranges.amazonaws.com/ip-ranges.json -o ip-ranges.json
+curl https://ip-ranges.amazonaws.com/ip-ranges.json -o ip-ranges-aws.json
+curl https://www.gstatic.com/ipranges/cloud.json -o ip-ranges-gcp.json
 node ip-range-count.js
 ```
 
--- ip-range-count.js
+++ ip-range-count.js
@@ -1,15 +1,25 @@
-const { prefixes: ranges } = require('./ip-ranges.json');
-const rangeSizes = ranges.map(range => Number.parseInt(range.ip_prefix.split('/')[1], 10));
-console.info(
-  'Occurrences of CIDR range sizes:',
-  rangeSizes.reduce((acc, rangeSize) => ({ ...acc, [rangeSize]: (acc[rangeSize] ? acc[rangeSize] : 0) + 1 }), {}),
-);
-console.info(
-  'Number of ranges:',
-  ranges.length,
-);
-const rangeSizeToNumberOfIPs = (rangeSize) => { return Math.pow(2, 32 - rangeSize) }
-console.info(
-  'Number of IP addresses:',
-  rangeSizes.reduce((acc, rangeSize) => acc + rangeSizeToNumberOfIPs(rangeSize), 10),
-);
+const { prefixes: awsRanges } = require('./ip-ranges-aws.json');
+const awsRangeSizes = awsRanges.map(range => Number.parseInt(range.ip_prefix.split('/')[1], 10));
+
+const { prefixes: gcpRanges } = require('./ip-ranges-gcp.json');
+const gcpRangeSizes = gcpRanges.filter(range => range.ipv4Prefix !== undefined).map(range => Number.parseInt(range.ipv4Prefix.split('/')[1], 10));
+
+const analyizeRanges = (rangeSizes) => {
+  console.info(
+    'Occurrences of CIDR range sizes:',
+    rangeSizes.reduce((acc, rangeSize) => ({ ...acc, [rangeSize]: (acc[rangeSize] ? acc[rangeSize] : 0) + 1 }), {}),
+  );
+  console.info(
+    'Number of ranges:',
+    rangeSizes.length,
+  );
+  const rangeSizeToNumberOfIPs = (rangeSize) => { return Math.pow(2, 32 - rangeSize) }
+  console.info(
+    'Number of IP addresses:',
+    rangeSizes.reduce((acc, rangeSize) => acc + rangeSizeToNumberOfIPs(rangeSize), 10),
+  );
+}
+console.info('---\nAWS\n---')
+analyizeRanges(awsRangeSizes);
+console.info('---\nGCP\n---')
+analyizeRanges(gcpRangeSizes);
